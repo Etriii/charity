ca40766fa013cb78ac81b57f76e4f8a0b96a47a3
Alex, Jr. A. Aparece <alexarnaizaparece@gmail.com>
2025-08-17 05:46:25 +0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <h1 className="text-9xl font-bold">
+        Palautot Micah
+      </h1>
+    </div>
+  );
+}
